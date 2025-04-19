a3e15084a9cbf46e511b3ca9ed77c13f5a64f408
DARKday <onexz.kontakt@gmail.com>
2025-04-20 01:27:45 +0200

    .

--- zadanie 6/6.1.py
+++ zadanie 6/6.1.py
@@ -1,5 +1,18 @@
 file = open('dane6przyklad.txt','r')
 linijki = file.readlines()
+p_minimalne = 0
 
 for linijka in linijki:
-    
+    linjika = linijka.strip()
+    for cyfra in linijka:
+        cyfra = int(cyfra.strip())
+        for p in range(2,10):
+            if cyfra == p:
+                p_minimalne +=1
+
+                break
+
+
+
+
+print(p_minimalne)
