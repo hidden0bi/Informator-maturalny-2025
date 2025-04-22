3d4596ad9a27d03573b1e53ca443babd0e50c701
DARKday <onexz.kontakt@gmail.com>
2025-04-22 20:02:58 +0200

    .

--- zadanie 6/6.1.py
+++ zadanie 6/6.1.py
@@ -1,18 +1,21 @@
 file = open('dane6przyklad.txt','r')
+#file = open('dane6.txt','r')
 linijki = file.readlines()
-p_minimalne = 0
-
-for linijka in linijki:
-    linjika = linijka.strip()
-    for cyfra in linijka:
-        cyfra = int(cyfra.strip())
-        for p in range(2,10):
-            if cyfra == p:
-                p_minimalne +=1
-
-                break
 
 
-
-
-print(p_minimalne)
+for p in range(2,11):
+    licznik = 0
+    for linijka in linijki:
+        linijka = linijka.strip()
+        p_minus = False
+        wieksze_od = False
+        for cyfra in linijka:
+            cyfra = int(cyfra)
+            if cyfra == p-1:
+                p_minus = True
+            if cyfra >= p:
+                wieksze_od = True
+                break # jezeli sie znajdzie, to wychodzi z petli, zeby nie zaliczac 10 razy tego samego, nie zmieni wyniku, to optymalizacja
+        if p_minus and not wieksze_od:
+            licznik += 1
+    print(p, licznik)
